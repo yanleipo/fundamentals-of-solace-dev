f479d7ca2df1ff654b0e948f1d339e0c22281ac1
Leipo Yan <leipo.yan@u-blox.com>
2019-07-24 15:09:10 +0800

    Merge auth-server and web-server into single SpringBoot application.

---
+++ web-app-server/src/main/java/com/solace/chat/application/auth/server/AuthServer.java
@@ -0,0 +1,16 @@
+package com.solace.chat.application.auth.server;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class AuthServer implements CommandLineRunner {
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
--- web-app-server/src/main/java/com/solace/chat/application/web/server/ChatServer.java
+++ web-app-server/src/main/java/com/solace/chat/application/web/server/ChatServer.java
@@ -2,7 +2,11 @@ package com.solace.chat.application.web.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.solace.chat.application.auth.server.AuthServer;
+
+@ComponentScan(basePackages = {"com.solace.chat.application.*"})
 @SpringBootApplication
 public class ChatServer {
 
