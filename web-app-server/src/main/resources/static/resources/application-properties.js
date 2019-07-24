04fed8c47f49fe319a9345acd254c640deeaa600
Leipo Yan <leipo.yan@u-blox.com>
2019-07-24 15:27:52 +0800

    Remove property files from gitignore

---
+++ web-app-server/src/main/resources/static/resources/application-properties.js
@@ -0,0 +1,9 @@
+connectOptions = {
+	sBROKERURL: 'ws://ubx.ddns.net:580',
+	sVPN: 'default',
+	sUSERNAME: 'default',
+	sPASSWORD: 'default',
+	sPublishTopic: 'solace/chat',
+	sSubscribeTopic: 'solace/chat',
+	sReceiveQueue: 'SOLACE_CHAT'
+}
